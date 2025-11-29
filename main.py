8a93fe69c2f284ad858b57dd74e4170c319a9835
nabeelah.okanlawon@hpe.com <nabeelah.okanlawon@hpe.com>
2025-11-29 13:09:28 +0000

    updated to load env?

---
+++ main.py
@@ -0,0 +1,13 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+env = os.getenv("API_KEY")
+if not env:
+    raise ValueError("API_KEY not found in environment variables")
+
+response = requests.get("https://api.weatherapi.com/v1/current.json?key="+ env + "&q=London&aqi=no")
+
+print(response.json())
